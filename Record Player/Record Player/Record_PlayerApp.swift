2d273e20940d940e2d094b3536e0a9df90653446
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-29 18:27:14 +0300

    Record Player init

---
+++ Record Player/Record Player/Record_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Record_PlayerApp.swift
+//  Record Player
+//
+//  Created by Omer Cagri Sayir on 29.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Record_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
