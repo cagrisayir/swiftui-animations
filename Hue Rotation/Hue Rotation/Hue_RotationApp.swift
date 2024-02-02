bfe6ea86acb0d346fde74d6dca8d67c00c3a21f0
Omer Cagri Sayir <omercagris@gmail.com>
2024-02-02 18:23:52 +0300

    Hue rotation init

---
+++ Hue Rotation/Hue Rotation/Hue_RotationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hue_RotationApp.swift
+//  Hue Rotation
+//
+//  Created by Omer Cagri Sayir on 2.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Hue_RotationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
